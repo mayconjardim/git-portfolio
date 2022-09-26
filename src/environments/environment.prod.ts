af2cc7a5545c9ebdb7e524fc5833ccd9fed911a9
Maycon Jardim <mayconsmill@gmail.com>
2022-09-26 20:42:20 -0300

    github service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   username: 'mayconjardim',
+  apiUrl: 'https://api.github.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   username: 'mayconjardim',
+  apiUrl: 'https://api.github.com',
 };
 
 /*
