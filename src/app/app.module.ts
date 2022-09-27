22722a1aab7a582facf2991639488984a2e263e4
Maycon Jardim <mayconsmill@gmail.com>
2022-09-27 20:29:35 -0300

    mostrando repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonalInfoComponent, PanelComponent],
+  declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/repositories/repositories.component.ts
@@ -0,0 +1,22 @@
+import { GithubService } from './../github.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { Repository } from '../repository';
+
+@Component({
+  selector: 'app-repositories',
+  templateUrl: './repositories.component.html',
+  styleUrls: ['./repositories.component.scss'],
+})
+export class RepositoriesComponent implements OnInit {
+  repos$: Observable<Repository[]> | undefined;
+
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.repos$ = this.githubService
+      .getRepos()
+      .pipe(map((repos) => repos.filter((repo) => !repo.fork)));
+  }
+}
